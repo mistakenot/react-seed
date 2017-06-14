2046531189e4c8eaa8955b6fc0f7431230d5326c
charlie.daly <kss_charlied@dunnhumby.com>
2017-06-14 18:18:57 +0100

    Zone service.

--- src/services/zones/zones.service.ts
+++ src/services/zones/zones.service.ts
@@ -1,11 +1,14 @@
 import {Service} from "../";
 import { Action, Dispatch } from "utils/actions";
+import * as HttpActions from "../http/http.actions";
+import * as Actions from "./zones.actions";
 
 export class ZoneService implements Service {
     onAction(action: Action, dispatch: Dispatch) {
         switch (action.type) {
-            
+            case (Actions.Types.requestZones): {
+                dispatch(HttpActions.httpRequestAction("GET", "/zones"));
+            }
         }
     }
-
 }
