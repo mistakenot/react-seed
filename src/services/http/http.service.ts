32559fd1759e2f5154b3b4efd508d27218c46d2c
charlie.daly <kss_charlied@dunnhumby.com>
2017-06-16 15:17:45 +0100

    Added env file for absolute imports.

--- src/services/http/http.service.ts
+++ src/services/http/http.service.ts
@@ -1,12 +1,12 @@
 import * as request from "request";
-import { Action, ServiceBase, Dispatch } from "utils";
+import { Action, ServiceBase, Dispatch } from "../../utils";
 import * as Actions from "./http.actions";
 
 export class HttpService extends ServiceBase {
     constructor(dispatch: Dispatch) {
         super(dispatch);
     }
-    
+
     onAction(action: Action) {
         switch (action.type) {
             case (Actions.Types.request): {
